f92ca00195bcb73ec46a775bf1054d897a6ee24c
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-20 20:18:22 -0700

    pseudo code

---
+++ javascript/app.js
@@ -0,0 +1,28 @@
+// topic theme and make an array
+var topics = []
+
+// function to create new buttons from array
+
+// loops through the array to create buttons
+
+// user clicks on a generated button, which generates 10 static, non-animated gif images from the GIPHY API
+// add queryURL here
+// ajax call needed
+// console.log(response) to check
+
+// under every gif, display it's rating
+
+// add states of animate and still 
+
+// image is appended to the div
+
+// rating is appended to the div
+
+// new images will be placed at the top of page
+
+// form takes the value from the input box and adds it into the topics  array
+
+// buttonGenerator function is called that takes each topic in the array remakes the buttons on the page
+
+
+
